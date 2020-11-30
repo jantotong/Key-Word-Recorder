a7a934725d97f7c5678d5c488d545285aeb541f1
jantotong <tongjantoo@gmail.com>
2020-11-30 10:12:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -62,5 +62,5 @@ def upload():
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', threaded=True, port=80)
-    app.run(threaded=True, port=80)
+    app.run(host='0.0.0.0', threaded=True, port=80)
+    # app.run(threaded=True, port=80)
